e239a1965dcf23c974b47b7655c38d41dbb5a412
Mumuni Mohammed <mumunim10@gmail.com>
2021-10-17 19:37:32 +0000

    Added viewsets and routers for User

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,8 @@ from .views import PostList, PostDetail
 
 
 urlpatterns = [
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
     path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
 ]
